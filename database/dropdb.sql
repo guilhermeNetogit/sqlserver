7c398f4339d48cbc7475839ad33c9ff5bfe3067d
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-04-02 20:40:45 -0300

    feat(): add new sql database statemet

---
+++ database/dropdb.sql
@@ -0,0 +1,4 @@
+/*Warning!! Be careful before dropping a database! 
+Dropping a database deletes the database and all its content (tables, views, stored procedures, and data)!
+The following SQL statement drops an existing database named "test_Database":*/
+DROP DATABASE test_Dataabase;
