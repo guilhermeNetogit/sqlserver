faf71b8f59bc54c6aa0b43344d22348eb79871f4
arauj0duy <m.duda.araujo2012@gmail.com>
2026-04-04 12:20:11 -0300

    feat(new SQL): add SQL statement

---
+++ database/alter-table/alterColumnDatatype.sql
@@ -0,0 +1,5 @@
+-- To modify the data type, size or constraints of a column in a table, use the following syntax:
+-- Syntax
+
+ALTER TABLE table_name
+ALTER COLUMN column_name new_datatype constraint;
