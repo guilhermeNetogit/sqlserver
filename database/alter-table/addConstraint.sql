99fa892bfe6e31e9d35637d8b604220fb4cb4134
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-04-07 11:45:03 -0300

    chore(text): complement the explanatory text

--- database/alter-table/addConstraint.sql
+++ database/alter-table/addConstraint.sql
@@ -9,3 +9,25 @@ ADD CONSTRAINT constraint_name constraint_definition;
 
 ALTER TABLE Members
 ADD CONSTRAINT CHK_Age CHECK (Age >= 18);
+
+/*SQL constraints are rules for data in a table.
+Constraints are used to prevent insertion of invalid data in a table,
+  and ensures the accuracy and reliability of the data in the table.
+  If there is any violation between the constraint and the data action, the action is aborted.
+
+Constraints can be specified in two ways:
+
+When a table is created (through the CREATE TABLE statement)
+After a table is created (through the ALTER TABLE statement)
+
+SQL Constraint Types
+The following constraints are commonly used in SQL:
+
+NOT NULL - Ensures that a column cannot have a NULL value
+UNIQUE - Ensures that all values in a column are unique
+PRIMARY KEY - Uniquely identifies each row in a table (a combination of a NOT NULL and UNIQUE)
+FOREIGN KEY - Establishes a link between data in two tables, and prevents action that will destroy the link between them
+CHECK - Ensures that the values in a column satisfies a specific condition
+DEFAULT - Sets a default value for a column if no value is specified
+CREATE INDEX - Creates indexes on columns to retrieve data from the database faster
+*/
